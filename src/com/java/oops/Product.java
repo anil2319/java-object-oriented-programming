b26f30aaa35305299735214b7f30cd44ff92a847
anil2319 <mudhirajanil325@gmail.com>
2026-04-27 12:54:14 +0530

    initial-commit

---
+++ src/com/java/oops/Product.java
@@ -0,0 +1,13 @@
+package com.java.oops;
+
+public class Product {
+    int id;
+    String name;
+    int reviewsCount;
+    boolean isAvailable;
+    String company;
+    String category;
+    float ratings;
+    long maxRetailPrice;
+    byte discountPercentage;
+}
